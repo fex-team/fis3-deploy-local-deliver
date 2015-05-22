8b1542faae4461e9e9306e5935922328bb4b520f
xiangshouding <fansekey@gmail.com>
2015-05-22 10:51:52 +0800

    fix

--- index.js
+++ index.js
@@ -66,37 +66,8 @@ function deliver(output, release, content, file, callback){
     callback();
 }
 
-function upload(receiver, to, release, content, file, callback){
-    var subpath = file.subpath;
-    fis.util.upload(
-        //url, request options, post data, file
-        receiver, null, { to : to + release }, content, subpath,
-        function(err, res){
-            if(err || res.trim() != '0'){
-                fis.log.error('upload file [' + subpath + '] to [' + to +
-                    '] by receiver [' + receiver + '] error [' + (err || res) + ']');
-            } else {
-                var time = '[' + fis.log.now(true) + ']';
-                process.stdout.write(
-                    ' - '.green.bold +
-                    time.grey + ' ' + 
-                    subpath.replace(/^\//, '') +
-                    ' >> '.yellow.bold +
-                    to + release +
-                    '\n'
-                );
-                callback();
-            }
-        }
-    );
-}
-
 module.exports = function (dest, file, content, settings, callback) {
     var root = fis.project.getProjectPath();
     var to = normalizePath(dest.to, root);
-    if(settings && settings.receiver) {
-        upload(settings.receiver, to, dest.release, content, file, callback);
-    } else {
-        deliver(to, dest.release, content, file, callback);
-    }
+    deliver(to, dest.release, content, file, callback);
 };
